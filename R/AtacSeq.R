4eddf0cf9936da9d11b8852d4469eab273ddd9d5
aiminy <jlfmssm@gmail.com>
2017-09-10 13:00:09 -0400

    add installAtacSeq

---
+++ R/AtacSeq.R
@@ -0,0 +1,12 @@
+
+# bsub -P bbc -J "RunSppR" -o %J.RunSppR.log -e %J.RunSppR.err -W 72:00 -n 32 -q parallel -R 'rusage[mem= 16000 ] span[ptile= 16 ]' -u aimin.yan@med.miami.edu R -e 'library(AtacSeq);AtacSeq:::useRunSppR("hg19","/scratch/projects/bbc/aiminy_project/AtacSeq")'
+
+installAtacSeq <- function(genome,DATA.DIR)
+{
+
+  if(!dir.exists(DATA.DIR)){dir.create(DATA.DIR,recursive = TRUE)}
+  cmd0= "bash ~/atac_dnase_pipelines/install_genome_data.sh"
+  cmd1= paste0(cmd0,genome,DATA.DIR,seperate=" ")
+  system(cmd1)
+
+}
