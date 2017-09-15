03addb30a85edb34de8d8664ee360a0df3653895
aiminy <jlfmssm@gmail.com>
2017-09-15 14:15:25 -0400

    update bash

--- inst/bin/bash/testAtacSeq.sh
+++ inst/bin/bash/testAtacSeq.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-TITLE=AtacTest;SPECIES=hg19;
+TITLE=AtacTest2;SPECIES=hg19;
 FASTQ1_1=/nethome/axy148/bioinf525/SRR891268.1.fq.gz
 FASTQ1_2=/nethome/axy148/bioinf525/SRR891268.2.fq.gz
 WORK=/scratch/projects/bbc/aiminy_project/$TITLE; mkdir -p $WORK; cd $WORK
-bds_scr $TITLE /home/axy148/atac_dnase_pipelines/atac.bds -species $SPECIES -nth 8 -fastq1_1 $FASTQ1_1 -fastq1_2 $FASTQ1_2
+bds /home/axy148/atac_dnase_pipelines/atac.bds -species $SPECIES -nth 8 -fastq1_1 $FASTQ1_1 -fastq1_2 $FASTQ1_2 -out_dir $WORK
